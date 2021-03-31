bcbd77243b47ca30a609f06c3efd8b086cb50511
sacha.van.weeren <sacha.van.weeren@gmail.com>
2021-03-31 14:53:48 +0200

    Added function division to calculator.py

--- calculator.py
+++ calculator.py
@@ -13,3 +13,9 @@ def subtract(first_term, second_term):
 
 def multiply(first_term, second_term):
     return first_term * second_term
+
+def division(numerator, denumerator):
+	if denumerator == 0:
+            print("Can divide by zero")
+        else:
+            return numerator/denumerator
