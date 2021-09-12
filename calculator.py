15d8003fd3aab70fd96f71dca17449a1ae3f9f14
longtongster <sacha.van.weeren@gmail.com>
2021-09-12 17:36:44 +0200

    added print

--- calculator.py
+++ calculator.py
@@ -4,7 +4,6 @@ Calculator library containing basic math operations.
 Code taken from real python website
 """
 
-
 def add(first_term, second_term):
     return first_term + second_term
 
@@ -14,6 +13,7 @@ def subtract(first_term, second_term):
 
 
 def multiply(first_term, second_term):
+    print("sacha")
     return first_term * second_term
 
 
